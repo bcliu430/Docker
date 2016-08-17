04bce97f0c0829a7c47490649e3d34433ae1ccd7
root <lbc0430@gmail.com>
2016-08-17 01:03:15 -0400

    create run cmd

---
+++ Doc_cmd.sh
@@ -0,0 +1,12 @@
+#!/bin/bash
+
+set -euxo pipefail
+
+if [ "$1" == "" ] || [ "$2" == "" ]; then
+	echo "usage: $0 <docker_name> <cmd>"
+	exit
+fi
+
+PID=$(docker inspect --format {{.State.Pid}} $1)
+
+ nsenter --target $PID --mount --uts --ipc --net --pid     ${*:2} 2>&1 | sed "s/^/[$1] /"
---
+++ Doc_cmds.sh
@@ -0,0 +1,13 @@
+et -euo pipefail
+
+SCRIPT_RUN_SINGLE="/root/docker/Docker/Doc_cmd.sh"
+
+if [ "$#" -lt 4 ]; then
+	echo "usage: $0 <prefix> <start> <stop> <cmd>"
+	exit
+fi
+
+for i in `seq $2 $3`; do
+	( $SCRIPT_RUN_SINGLE $1$i ${*:4}) &
+done
+wait
